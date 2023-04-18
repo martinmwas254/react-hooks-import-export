0e2fc37c825c169e82f152a26df062435a98834a
martinmwas254 <martinmwas264@gmail.com>
2023-04-18 19:32:04 +0300

    complete assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,14 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
-
-function ColoradoStateParks() {
+import MesaVerde from "./parks/MesaVerde";
+import { wildlife,trees } from "./parks/RockyMountain";
+export default function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (<>
+  <h1>Colorado State Parks!</h1>
+  <MesaVerde/>
+  {trees}
+  {wildlife()}
+  </>)
 }
